224b0c6140ea84703556b0ad025664ebe277b11e
dug22 <dougprograms0617@gmail.com>
2024-12-11 12:39:29 -0500

    Update ollama_complete_setup.sh

--- scripts/ollama_complete_setup.sh
+++ scripts/ollama_complete_setup.sh
@@ -2,7 +2,7 @@ echo "Installing Ollama on your environment"
 curl -fsSL https://ollama.com/install.sh | sh
 echo "Starting Ollama Service"
 python path/to/the/ollama_thread.py
-echo "Pulling the specified Ollama mode"
+echo "Pulling the specified Ollama model"
 ollama pull <model_type>
 echo "Installing LangChain Ollama's integration package"
 pip install langchain-ollama
